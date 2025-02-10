0ee19489e732088c944398f6aeeb60b90f1c86af
kerambitls <126450593+kerambitls@users.noreply.github.com>
2025-02-10 19:17:52 +0700

    add grid of categories

---
+++ src/components/CategoryInfo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-white shadow-md rounded-lg overflow-hidden cursor-pointer">
+    <img
+      src=""
+      :alt="alt"
+      class="w-full h-48 object-cover bg-gradient-to-r from-blue-300 to-green-300"
+    />
+    <div class="p-4">
+      <h2 class="text-xl font-semibold mb-2">{{ categoryName }}</h2>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+  categoryName: string
+  alt: string
+}>()
+</script>
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { useCategoriesStore } from '@/stores/categories.ts'
+import CategoryInfo from '@/components/CategoryInfo.vue'
+
+const categoriesStore = useCategoriesStore()
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="container mx-auto p-4">
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <CategoryInfo
+          v-for="[_, category] in categoriesStore.getCategories"
+          :key="category.id"
+          :category-name="category.name"
+          :alt="category.alt.main"
+        />
+      </div>
+    </div>
   </main>
 </template>
