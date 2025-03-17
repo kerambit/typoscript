bc25fa41440753698b3553cc30b285906c743df1
kerambitls <126450593+kerambitls@users.noreply.github.com>
2025-03-17 19:45:44 +0400

    add adding to cart on category page

---
+++ src/stores/cart.ts
@@ -0,0 +1,49 @@
+import { defineStore } from 'pinia'
+import { computed, ref, watch } from 'vue'
+
+export const useCartStore = defineStore('cart', () => {
+  const cart = ref<Map<number, number>>(new Map())
+  const storageKey = 'cart'
+
+  const savedCart = sessionStorage.getItem(storageKey)
+  if (savedCart) {
+    cart.value = new Map(JSON.parse(savedCart))
+  }
+
+  function addProduct(productId: number) {
+    const prevState = cart.value.get(productId)
+    if (!prevState) {
+      cart.value.set(productId, 1)
+      return
+    }
+
+    const newState = prevState + 1
+    cart.value.set(productId, newState)
+  }
+
+  function removeProduct(productId: number) {
+    if (!cart.value.has(productId)) {
+      return
+    }
+
+    cart.value.delete(productId)
+  }
+
+  function getCart() {
+    return computed(() => cart.value)
+  }
+
+  function clearCart() {
+    cart.value = new Map()
+  }
+
+  watch(
+    cart,
+    (newCart) => {
+      sessionStorage.setItem(storageKey, JSON.stringify(Array.from(newCart.entries())))
+    },
+    { deep: true },
+  )
+
+  return { addProduct, removeProduct, getCart }
+})
--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -4,35 +4,77 @@ import { useCategoriesStore } from '@/stores/categories.ts'
 import { useProductsStore } from '@/stores/products.ts'
 import { searchProducts } from '@/api/ecwidApi.ts'
 import { storeId, storeToken } from '@/config.ts'
+import { reactive } from 'vue'
+import { useCartStore } from '@/stores/cart.ts'
 
 const props = defineProps<{
   id: string
 }>()
+
 const route = useRoute()
 const categoriesStore = useCategoriesStore()
+const cartStore = useCartStore()
 const category = categoriesStore.getCategory(Number(props.id))
 
 const productsStore = useProductsStore()
 let products = productsStore.getProductsByCategoryId(Number(props.id))
-console.log(products.value)
 
 if (products.value === undefined) {
   searchProducts(storeId, storeToken, Number(props.id)).then((data) => {
     productsStore.setProducts(Number(props.id), data.items)
     products = productsStore.getProductsByCategoryId(Number(props.id))
-    console.log(products.value)
   })
+}
+
+const animateButtons = reactive<{ [key: number]: boolean }>({})
+
+function addToCart(productId: number) {
+  animateButtons[productId] = true
+  cartStore.addProduct(productId)
+  setTimeout(() => {
+    animateButtons[productId] = false
+  }, 500)
 }
 </script>
 
 <template>
   <div v-if="category !== undefined">
-    <h1>Category: {{ route.params.id }}</h1>
-    <p>{{ category.name }}</p>
+    <h1 class="text-2xl font-bold mb-4">{{ category.name }}</h1>
     <template v-if="products && products.length > 0">
-      <p v-for="product in products" :key="product.id">{{ product.name }}</p>
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div
+          v-for="product in products"
+          :key="product.id"
+          class="bg-white shadow-md rounded-lg overflow-hidden p-4"
+        >
+          <img :src="product.imageUrl" :alt="product.name" class="w-full h-48 object-cover mb-4" />
+          <h2 class="text-xl font-semibold mb-2">{{ product.name }}</h2>
+          <p class="text-lg font-bold mb-4">{{ product.price }} $</p>
+          <button
+            class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+            :class="{ bounce: animateButtons[product.id] }"
+            @click="addToCart(product.id)"
+          >
+            Buy
+          </button>
+        </div>
+      </div>
     </template>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+@keyframes bounce {
+  0%,
+  100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-10px);
+  }
+}
+
+.bounce {
+  animation: bounce 0.5s;
+}
+</style>
