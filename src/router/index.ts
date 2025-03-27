d88e8bd8e07ca9067293813f363fea0bca106b78
kerambitls <126450593+kerambitls@users.noreply.github.com>
2025-03-27 20:30:10 +0400

    change routing to work on gh pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import CategoryView from '@/views/CategoryView.vue'
 import ProductView from '@/views/ProductView.vue'
 import CartView from '@/views/CartView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory('/typoscript/'),
   routes: [
     {
       path: '/',
